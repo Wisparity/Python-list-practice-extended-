3a1b8a3f50a16e2319574b3300bea6ed99cebb77
Wisp <146360282+Wisparity@users.noreply.github.com>
2024-02-09 09:16:44 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+Summertime_Memories = ["Friend by Jeff and Casey Williams","Summer by Calvin Harris","Fall to Light by lazlo","Kokiri Forest by Koji Kondo"]
+Summertime_Memories.remove("Summer by Calvin Harris")
+Summertime_Memories.insert(4,'Cloud 9 by Beach Bunny')
+print(len(Summertime_Memories))
+if "Friend by Jeff and Casey Williams" in Summertime_Memories:
+    print("True")
+else:
+    print("false")
